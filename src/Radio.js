ae5d391a01d4958846cf76df098a160db5c27cf8
Robert Reinhard <robert.reinhard@bukwild.com>
2019-05-31 20:36:14 -0700

    Adding tooltips with the hex value

--- src/Radio.js
+++ src/Radio.js
@@ -1,27 +1,32 @@
 import React from 'react'
-import { TextLink } from '@contentful/forma-36-react-components'
+import { TextLink, Tooltip } from '@contentful/forma-36-react-components'
 
 // Export compoent
 export default function Radio (props) {
 	return (
 		<div className='radio'>
-			<span className='description' onClick={() => props.onClick(props.name) }>
-		
-				{/* Radio input */}
-				<input type='radio' checked={ props.checked } readOnly />
-				
-				{/* Color name */}
-				<span className='name'>{ props.name }</span>
-				
-				{/* Color swatch */}
-				{ props.color 
-					? <span className='swatch' style={{ background: props.color }} /> 
-					: null }
-			</span>
+			<Tooltip place='right' content={ props.color }>
+				<span className='description' onClick={() => props.onClick(props.name) }>
+			
+					{/* Radio input */}
+					<input type='radio' checked={ props.checked } readOnly />
+					
+					{/* Color name */}
+					<span className='name'>{ props.name }</span>
+					
+					{/* Color swatch */}
+					<span className='swatch' style={{ background: props.color }}/>
+			
+				</span>
+			</Tooltip>
 				
 			{/* Clear link */}
 			{ props.checked 
-				? <TextLink onClick={() => props.onClick()}>Clear</TextLink> 
+				? <TextLink 
+					className='clear' 
+					onClick={() => props.onClick()}>
+						Clear
+					</TextLink> 
 				: null }
 		</div>
 	)
